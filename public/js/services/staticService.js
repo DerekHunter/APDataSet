45990d193e50dbffff592eb27b7e263c5a5beb82
Derek Hunter <Derek@ifit.com>
2015-08-28 13:27:05 -0600

    Adding some more pictures, stuff is broken though

--- public/js/services/staticService.js
+++ public/js/services/staticService.js
@@ -4,7 +4,8 @@ angular.module('ApData').factory('StaticDataService', function() {
    StaticData.champions =[
      {id:"1", display: "Veigar"},
      {id:"2", display: "Riven"},
-     {id:"3", display:"Jinx"}
+     {id:"3", display:"Jinx"},
+     {id:"4", display:"Rengar"}
    ];
 
    StaticData.statistics = ["Kills", "Deaths", "Assists"]
