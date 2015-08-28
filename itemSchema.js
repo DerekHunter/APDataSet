16f1b37bc10c93f3c822cfdb52d83df4b7498c98
DerekHunter <derek.hunter@aggiemail.usu.edu>
2015-08-28 17:02:00 -0600

    Removing schema not using mongoose

--- itemSchema.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var itemSchema = new Schema({
-  itemId: String,
-  averageBought: Number
-  region: String,
-  ranked: Boolean,
-})
