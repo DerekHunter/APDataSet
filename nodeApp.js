14ce66ab96cf0764724f9af0e21156dabd696c96
Derek Hunter <Derek@ifit.com>
2015-08-28 15:52:54 -0600

    idk what i changed, but it's working so here you go

--- nodeApp.js
+++ nodeApp.js
@@ -46,8 +46,6 @@ app.use('/api', router);
 
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://localhost:%s', port);
 });
